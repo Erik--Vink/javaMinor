5b7b3aea60f83a73f6fe58aef603f17649b06197
Erik--Vink <jhvink@student.avans.nl>
2016-09-08 11:07:39 +0200

    User unqiue + gitignore

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ function init(mongoose) {
 
     var userSchema = new mongoose.Schema(
         {
-            number: {type: Number, required: true },
+            number: {type: Number, required: true, unique: true },
             name: {type: String, required: true },
             role: {type: String, enum: ['student', 'docent'], required: true},
             class: { type: mongoose.Schema.Types.ObjectId, ref: "Group"}
